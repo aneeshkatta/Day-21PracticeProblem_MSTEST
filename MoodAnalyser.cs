562eca3c7741a31074a8cc669a957e6fb3e06f7b
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-30 00:15:07 +0530

    Add files via upload
    uc2 added

---
+++ MoodAnalyser.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21_MSMODEANALYSER_ASGMNT1
+{
+    public class MoodAnalyser
+    {
+
+        public string analyseMood(string message)
+        {
+
+            if (message.Equals(string.Empty))
+            {
+                return "Mood should not be empty";
+            }
+            else if (message.Contains("Sad"))
+            { return "Sad"; }
+            else if (message.Contains("Null"))
+            { return "Happy"; }
+            else
+            {
+                return "Happy";
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+        }
+
+    }
+    
+        
+    
+
+
+
+
+}
